14749bc1baffa14c5db8daee4c314fd4957112a0
Fabricio Lin <fabriciolin70@gmail.com>
2024-09-20 20:53:07 -0400

    Se agrega conexion a la base de datos

--- fitsync/Program.cs
+++ fitsync/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Repository.Data.Conexion;
 
 namespace fitsync
 {
@@ -14,9 +15,30 @@ namespace fitsync
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Login());
+
+            try
+            {
+                // Crear una instancia de la clase Conexion
+                Conexion conexion = new Conexion();
+
+                // Probar abrir la conexión
+                conexion.AbrirConexion();
+
+                // Si llegamos aquí, la conexión fue exitosa
+                Console.WriteLine("Conexión a la base de datos fue exitosa.");
+
+                // Probar cerrar la conexión
+                conexion.CerrarConexion();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocurrió un error: " + ex.Message);
+            }
+
+            Console.ReadKey(); // Mantener la consola abierta hasta que presiones una tecla
         }
     }
 }
